9eb1a65f4f479f63021dcfe9894d2e8105831a83
iburtila <iburtila@yahoo.com>
2025-02-02 21:42:59 +0200

    Commit end sesion 1

---
+++ src/main/java/VariabilaMetoda/Student.java
@@ -0,0 +1,33 @@
+package VariabilaMetoda;
+
+public class Student {
+    //Clasa = sablon care defineste proprietatile si comportamentul unei entitati specifice
+    // intr-un fisier Java putem avea mai multe clase diferentiate pe baza numelui
+    // nu este bine sa avem mai multe clase cu acelasi nume
+
+    //intr-o clasa putem defini propietatile si actiunile entitatii
+
+    //variabila = proprietatea unei clase
+
+    //variabilele pot fi de doua feluri: globale si locale
+
+    //variabile globale =  sunt vizibile peste tot in proiect
+
+    //variabile locale =  sunt vizibile doar in locul unde sunt declarate
+
+    // ca sa definim o variabila globala:
+    //  -> accesscontrol (public)/
+    //  -> tip variabila /nume variabila
+    //cand definim o variabila nu este obligatoriu sa ii dam o valoare
+
+    public String Nume;
+    public String Prenume;
+    public Integer Varsta;
+    public String Adress;
+    public Double Inaltime;
+    public Float Greutate;
+    public Character Sex;
+    public Boolean AreDiploma;
+
+
+}
